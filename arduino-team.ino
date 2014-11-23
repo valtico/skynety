00e36ca17e8a59b84bbf93df6459f2a713dd189f
Valter Strods <valters.strods@gmail.com>
2014-11-23 13:32:50 +0200

    Screw this, branch off
    We are making our own branch of the code in front of as just like we
    will need for our own bot.

--- arduino-team.ino
+++ arduino-team.ino
@@ -1,37 +1,17 @@
 #define PIN_LED 13
-#define PIN_LED_A 15
-#define PIN_LED_B 16
-#define PIN_LED_C 17
 
-#define PIN_BTN_1 11
-#define PIN_BTN_2 A1
-
-#define PIN_DIP_SWITCH_1 0
-#define PIN_DIP_SWITCH_2 1
-#define PIN_DIP_SWITCH_3 2
-#define PIN_DIP_SWITCH_4 3
+#define PIN_PROXIMITY_LEFT A8
+#define PIN_PROXIMITY_BOTTOM A6
+#define PIN_PROXIMITY_RIGHT A4
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_LED_A, OUTPUT);
-  pinMode(PIN_LED_B, OUTPUT);
-  pinMode(PIN_LED_C, OUTPUT);
 
-  pinMode(PIN_BTN_1, INPUT_PULLUP);
-  pinMode(PIN_BTN_2, INPUT_PULLUP);
-
-  pinMode(PIN_DIP_SWITCH_1, INPUT_PULLUP);
-  pinMode(PIN_DIP_SWITCH_2, INPUT_PULLUP);
-  pinMode(PIN_DIP_SWITCH_3, INPUT_PULLUP);
-  pinMode(PIN_DIP_SWITCH_4, INPUT_PULLUP);
+  pinMode(PIN_PROXIMITY_LEFT, INPUT);
+  pinMode(PIN_PROXIMITY_BOTTOM, INPUT);
+  pinMode(PIN_PROXIMITY_RIGHT, INPUT);
 }
 
 void loop() {
-  mirrorBtnLed(PIN_BTN_1, PIN_LED_A);
-  mirrorBtnLed(PIN_BTN_2, PIN_LED_B);
-}
-
-void mirrorBtnLed(int buttonPin, int ledPin) {
-  digitalWrite(ledPin, digitalRead(buttonPin) ? LOW : HIGH);
 }
 
