658e77a946524662a50bd5da8662d7311105aaa3
Valter Strods <valters.strods@gmail.com>
2014-11-23 13:13:39 +0200

    Add some switches and buttons or something

--- arduino-team.ino
+++ arduino-team.ino
@@ -3,11 +3,27 @@
 #define PIN_LED_B 16
 #define PIN_LED_C 17
 
+#define PIN_BTN_1 11
+#define PIN_BTN_2 A1
+
+#define PIN_DIP_SWITCH_1 0
+#define PIN_DIP_SWITCH_2 1
+#define PIN_DIP_SWITCH_3 2
+#define PIN_DIP_SWITCH_4 3
+
 void setup() {
   pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_LED_A, OUTPUT);
   pinMode(PIN_LED_B, OUTPUT);
   pinMode(PIN_LED_C, OUTPUT);
+
+  pinMode(PIN_BTN_1, INPUT_PULLUP);
+  pinMode(PIN_BTN_2, INPUT_PULLUP);
+
+  pinMode(PIN_DIP_SWITCH_1, INPUT_PULLUP);
+  pinMode(PIN_DIP_SWITCH_2, INPUT_PULLUP);
+  pinMode(PIN_DIP_SWITCH_3, INPUT_PULLUP);
+  pinMode(PIN_DIP_SWITCH_4, INPUT_PULLUP);
 }
 
 void loop() {
