a7094bf8650aa7210380e9d1046eb665e0d8bb0e
Valter Strods <valters.strods@gmail.com>
2014-11-23 14:06:50 +0200

    Add stuff back because magic and final product

--- arduino-team.ino
+++ arduino-team.ino
@@ -1,8 +1,21 @@
 #define PIN_LED 13
+#define PIN_LED_A 15
+#define PIN_LED_B 16
+#define PIN_LED_C 17
+
+#define PIN_BTN_1 11
+#define PIN_BTN_2 A1
+
+#define PIN_DIP_SWITCH_1 0
+#define PIN_DIP_SWITCH_2 1
+#define PIN_DIP_SWITCH_3 2
+#define PIN_DIP_SWITCH_4 3
 
 #define PIN_PROXIMITY_LEFT A8
 #define PIN_PROXIMITY_BOTTOM A6
 #define PIN_PROXIMITY_RIGHT A4
+
+boolean doRun = false;
 
 void setup() {
   Serial.begin(9600);
@@ -10,28 +23,26 @@ void setup() {
 
   pinMode(PIN_LED, OUTPUT);
 
-  pinMode(PIN_PROXIMITY_LEFT, INPUT);
-  pinMode(PIN_PROXIMITY_BOTTOM, INPUT);
-  pinMode(PIN_PROXIMITY_RIGHT, INPUT);
+  pinMode(PIN_BTN_1, INPUT_PULLUP);
+  pinMode(PIN_BTN_2, INPUT_PULLUP);
 }
 
 void loop() {
-  int valProxLeft = analogRead(PIN_PROXIMITY_LEFT);
-  int valProxBot = analogRead(PIN_PROXIMITY_BOTTOM);
-  int valProxRight = analogRead(PIN_PROXIMITY_RIGHT);
+  if (!digitalRead(PIN_BTN_1)) doRun = true;
+  if (!digitalRead(PIN_BTN_2)) doRun = false;
 
-  Serial.print(" left=");
-  Serial.print(valProxLeft);
-  Serial.println();
+  if (doRun) {
+    mirrorProxLed(PIN_PROXIMITY_LEFT, PIN_LED_A);
+    mirrorProxLed(PIN_PROXIMITY_BOTTOM, PIN_LED_B);
+    mirrorProxLed(PIN_PROXIMITY_RIGHT, PIN_LED_C);
+  } else {
+    digitalWrite(PIN_LED_A, LOW);
+    digitalWrite(PIN_LED_B, LOW);
+    digitalWrite(PIN_LED_C, LOW);
+  }
+}
 
-  Serial.print(" bot=");
-  Serial.print(valProxBot);
-  Serial.println();
-
-  Serial.print(" right=");
-  Serial.print(valProxRight);
-  Serial.println();
-
-  delay(1000);
+void mirrorProxLed(int sensorPin, int ledPin) {
+  digitalWrite(ledPin, analogRead(sensorPin) < 200 ? HIGH : LOW);
 }
 
