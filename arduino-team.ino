b48efcd67fb6e39e73f4bb8a0ec2e2cd789baf7f
Valter Strods <valters.strods@gmail.com>
2014-11-23 12:57:39 +0200

    Add LED blink support

--- arduino-team.ino
+++ arduino-team.ino
@@ -1,6 +1,21 @@
+#define PIN_LED 13
+
 void setup() {
+  pinMode(PIN_LED, OUTPUT);
 }
 
 void loop() {
+  blinkPin(PIN_LED);
+}
+
+void blinkPin(int pin) {
+  blinkPin(pin, 1000);
+}
+
+void blinkPin(int pin, int delayMillis) {
+  digitalWrite(pin, HIGH);
+  delay(delayMillis / 2);
+  digitalWrite(pin, LOW);
+  delay(delayMillis / 2);
 }
 
