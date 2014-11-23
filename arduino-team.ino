2f76421d24b940d48dfc8111ed6eff28bdff9e0c
Valter Strods <valters.strods@gmail.com>
2014-11-23 13:03:05 +0200

    Set up lights for the sample robot
    We have a box to test on. Yay for samples.

--- arduino-team.ino
+++ arduino-team.ino
@@ -1,15 +1,24 @@
 #define PIN_LED 13
+#define PIN_LED_A 15
+#define PIN_LED_B 16
+#define PIN_LED_C 17
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_LED_A, OUTPUT);
+  pinMode(PIN_LED_B, OUTPUT);
+  pinMode(PIN_LED_C, OUTPUT);
 }
 
 void loop() {
   blinkPin(PIN_LED);
+  blinkPin(PIN_LED_A);
+  blinkPin(PIN_LED_B);
+  blinkPin(PIN_LED_C);
 }
 
 void blinkPin(int pin) {
-  blinkPin(pin, 1000);
+  blinkPin(pin, 500);
 }
 
 void blinkPin(int pin, int delayMillis) {
